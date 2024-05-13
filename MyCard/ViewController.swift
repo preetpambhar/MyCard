96b7ce4046327dd26c6fd2a359ade9530861d2b0
Preet Pambhar <preetpambhar73530@gmail.com>
2024-05-12 22:35:56 -0400

    Initial Commit

---
+++ MyCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCard
+//
+//  Created by Preet Pambhar on 2024-05-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
