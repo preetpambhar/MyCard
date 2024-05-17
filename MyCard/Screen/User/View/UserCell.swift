f8f1ebf980d0626332e83b0ef90f7794c1302096
Preet Pambhar <preetpambhar73530@gmail.com>
2024-05-17 02:05:13 -0400

    feat: updatae function, delete function

---
+++ MyCard/Screen/User/View/UserCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserCell.swift
+//  MyCard
+//
+//  Created by Preet Pambhar on 2024-05-14.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
